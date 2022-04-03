a16993fd17fd9da988db5ea30fe6368eac42cbaa
Bonapart <mibuchi773@mail.ru>
2022-04-03 22:51:36 +0600

    lab8 done

---
+++ lab8/shopback/api/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http.response import JsonResponse
+from api.models import Product, Category
+
+
+def get_products_list(request):
+    products_list = Product.objects.all()
+    products_list_json = [product.to_json() for product in products_list]
+    return JsonResponse(products_list_json, safe=False)
+
+def get_product(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist as e:
+        return JsonResponse({'message': str(e)}, status=400)
+
+    return JsonResponse(product.to_json())
+
+def get_categories_list(request):
+    categories_list = Category.objects.all()
+    categories_list_json = [category.to_json() for category in categories_list]
+    return JsonResponse(categories_list_json, safe=False)
+
+def get_category(request, category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist as e:
+        return JsonResponse({'message': str(e)}, status=400)
+
+    return JsonResponse(category.to_json())
---
+++ lab8/shopback/shopback/urls.py
@@ -0,0 +1,26 @@
+"""shopback URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from api.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/products/', get_products_list),
+    path('api/products/<int:product_id>', get_product),
+    path('api/categories/', get_categories_list),
+    path('api/categories/<int:category_id>/', get_category)
+]
