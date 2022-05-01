b6a054ab7ed6c7435c02d51a275398e1b5686b45
Bonapart <mibuchi773@mail.ru>
2022-05-01 17:53:44 +0600

    project

--- project/BuyNan/src/app/app-routing.module.ts
+++ project/BuyNan/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ItemsListComponent } from './items-list/items-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: AppComponent}, 
+  { path: 'products', component: ItemsListComponent}, 
+  { path: 'categories', component: ItemsListComponent}, 
+  { path: 'providers', component: AppComponent}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
